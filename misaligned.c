61c9fcd7063b88a20847e8c8257a74a378016f4a
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-31 20:57:53 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -18,14 +18,14 @@ void colormap(int Apairnumber);
 
 
 
-void colormap(int Apairnumber)
+color_pair colormap(int Apairnumber)
 {   
     color_pair LColor_Pair;
  
     LColor_Pair.Major_Color = majorColor[(Apairnumber-1)/5];
     LColor_Pair.Minor_Color = minorColor[(Apairnumber-1)%5];
  
-    printOnConsole(Apairnumber,LColor_Pair);
+    
 }
 
 void printOnConsole(int i, const color_pair AColor_Pair)
@@ -34,10 +34,20 @@ void printOnConsole(int i, const color_pair AColor_Pair)
 }
     
 int main() {
-   
+    
+    color_pair Color_Pair;
+    
     for(PairNumber=1; MAX_PAIRNUMBER >= PairNumber ; PairNumber++)
     {
-       colormap(PairNumber);
+       Color_Pair=colormap(PairNumber);
+       printOnConsole(Apairnumber,Color_Pair);
     }
     return 0;
+}
+test main()
+{
+    color_pair Color_Pair;
+    Color_Pair=colormap(5);
+    assert(LColor_Pair.Major_Color == "White");
+    assert(LColor_Pair.Minor_Color == "Slate"); 
 }
