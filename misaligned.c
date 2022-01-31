4f0e6fc298cd979aa29230868a6399157f7333e4
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-31 20:15:07 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define Max_Major_Color 5U
-#define Max_Minor_Color 5U
+ const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+ const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-/* This function will be check pair names aginst pair number befor getting print */
-
-void printOnConsole(int AIndex_i , const char* AmajorColor_c , const char* AminorColor_c)
+struct color_pair
 {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-   
-    /* Below assert function will be check each pair */
+    char* Major_Color;
+    char* Minor_Color;
+};
+
+color_pair colormap(int pairnumber)
+{
+    color_pair Lcolor_pair;
     
-    assert( majorColor[AIndex_i / Max_Major_Color] == AmajorColor_c);
-    assert( minorColor[AIndex_i % Max_Minor_Color] == AminorColor_c);
-     
-    printf("%d | %s | %s\n", AIndex_i , AmajorColor_c, AminorColor_c);
+    Lcolor_pair.Major_Color = majorColor[(pairnumber-1)\5];
+    Lcolor_pair.Minor_Color = minorColor[(pairnumber-1)%5];
+    
+    return Lcolor_pair;
 }
-
-
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-       for(j = 0; j < 5; j++) 
-       {
-           printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-           printOnConsole(i * 5 + j , majorColor[i] ,  minorColor[i]);
-        }
-    }
-    return i * j;
+printOnConsole(int i, const color_pair AColor_Pair)
+{
+    printf("%d, %s %s",i,AColor_Pair.Major_Color,AColor_Pair.Minor_Color);
 }
-
+    
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
-    printf("All is well (maybe!)\n");
+    color_pair Color_Pair;
+    int i;
+    for(i=1; 25 >= i ; i++)
+    {
+       Color_Pair =colormap(i);
+       printOnConsole(i,&Color_Pair);
+    }
+   // int result = printColorMap();
+   // assert(result == 25);
+   // printf("All is well (maybe!)\n");
     return 0;
 }
