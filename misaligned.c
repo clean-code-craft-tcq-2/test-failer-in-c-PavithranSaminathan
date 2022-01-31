8690cd12a305eeb4ec2c1306a614be3a94703a5f
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-01-31 20:20:25 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -19,7 +19,7 @@ color_pair colormap(int pairnumber)
     
     return Lcolor_pair;
 }
-printOnConsole(int i, const color_pair AColor_Pair)
+void printOnConsole(int i, const color_pair AColor_Pair)
 {
     printf("%d, %s %s",i,AColor_Pair.Major_Color,AColor_Pair.Minor_Color);
 }
@@ -30,7 +30,7 @@ int main() {
     for(i=1; 25 >= i ; i++)
     {
        Color_Pair =colormap(i);
-       printOnConsole(i,&Color_Pair);
+       printOnConsole(i,Color_Pair);
     }
    // int result = printColorMap();
    // assert(result == 25);
