e98f21b5262f8c052a056a191617f48423926a92
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-01 12:45:32 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -21,29 +21,40 @@ void printOnConsole(int i, const color_pair AColor_Pair)
     printf("\n %d, %s %s",i,AColor_Pair.Major_Color,AColor_Pair.Minor_Color);
 }
 
-color_pair colormap(int Apairnumber, color_pair AColor_Pair, void (*Print_Output)(int,color_pair))
+void colormap(int Apairnumber, color_pair AColor_Pair, void (*Print_Output)(int,color_pair))
 {   
     
     AColor_Pair.Major_Color = majorColor[(Apairnumber-1)/5];
     AColor_Pair.Minor_Color = minorColor[(Apairnumber-1)%5];
     
     Print_Output(Apairnumber,AColor_Pair);
-    
-    return AColor_Pair;
 }
 
+void TestFun(int Apairnumber,const char* Major_C , const char * Minor_C, const color_pair AColor_Pair)
+{
+    colormap(Apairnumber,AColor_Pair,Print_Output);   
+    
+    assert(AColor_Pair.Major_Color ==Major_C);
+    assert(AColor_Pair.Minor_Color ==Minor_C);
+}
 
     
 int main()
 {    
     color_pair Color_Pair;  
     
-    void (*Print_Output)(int,const color_pair)=&printOnConsole;
+    void (*Print_Output)(int,const color_pair)=printOnConsole;
+    
+    void (*AssertFun)(int,const char*,const char*,const color_pair) = TestFun;
+    
     
     for(PairNumber=1; MAX_PAIRNUMBER >= PairNumber ; PairNumber++)
     {
-       Color_Pair=colormap(PairNumber,Color_Pair,Print_Output);
-    //   printOnConsole(Apairnumber,Color_Pair);
+       Color_Pair=colormap(PairNumber,Color_Pair,Print_Output);    
     }
+    
+    AssertFun(2, "White","Orange",Color_Pair);
+    
+    
     return 0;
 }
