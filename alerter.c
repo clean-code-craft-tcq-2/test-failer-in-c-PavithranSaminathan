1d03b2bfa7b9acaeebd93e31fb62e0a4e06bdb21
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-01 16:45:22 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -6,33 +6,16 @@
 int alertFailureCount = 0;
 int Test_alertFailureCount=0;
 
+#define     Test            0u
+#define     PRODUCTION      1U
+
+#define     ENVIRONMENT     0U
+
 //Stub function for Network Alert in AlertInCelcius_Test testcase
 int NetworkAlertStub(float celcius)
 {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    if(THRESHOLD_VALUE > celcius)
-    {
-        return 500;
-    }
-    else
-    {
-        return 200;
-    }
-}
-
-// Test function for AlerterInCelcius
-void AlertInCelcius_Test(float farenheit)
-{
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = NetworkAlertStub(celcius);
-    if (returnCode != 200)
-    {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        Test_alertFailureCount += 0;
-    }
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);   
+    return 500;    
 }
 
 //Real network aleter function for alertInCelcius
@@ -51,25 +34,40 @@ int networkAlert(float celcius)
 }
 
 //This function will convert farenheit to celcius and test the alert
-void alertInCelcius(float farenheit)
+void alertInCelcius(float farenheit, int (*NetWorkAlert_FunP)(float celcius))
 {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlert(celcius);
+    int returnCode = NetWorkAlert_FunP(celcius);
     if (returnCode != 200)
     {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
-int main() {
-    AlertInCelcius_Test(400.5);
-    AlertInCelcius_Test(303.6);
-    assert(Test_alertFailureCount != 0);
-    printf("%d alerts failed.\n", Test_alertFailureCount);
+int main() 
+{
+    
+    int (*NetWorkAlert_FunP)(float celcius)[] ={networkAlert,networkAlertStub};
+    
+    #if (ENVIRONMENT == PRODUCTION)
+    
+    AlertInCelcius(400.5,networkAlert);
+    AlertInCelcius(303.6,networkAlert);
+    
+    #elif (ENVIRONMENT == TEST)
+    
+    AlertInCelcius(400.5,networkAlertStub);
+    AlertInCelcius(303.6,networkAlertStub);
+    
+    assert(alertFailureCount == 2);
+    
+    #endif
+    
+    printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
 }
